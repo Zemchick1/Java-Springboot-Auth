017c79411840757106f321744f258cedd4c292eb
Zem <zemchickpro@gmail.com>
2023-10-21 14:28:43 +0200

    init

---
+++ src/main/java/com/zemka/graphicscardservice/GraphicsCardServiceApplication.java
@@ -0,0 +1,13 @@
+package com.zemka.graphicscardservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphicsCardServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphicsCardServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zemka/graphicscardservice/GraphicsCardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zemka.graphicscardservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphicsCardServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
