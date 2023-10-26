bd0aa5e3abf95c91cd202a91a6e4e2602b68a257
Zem <zemchickpro@gmail.com>
2023-10-26 20:32:45 +0200

    OpenApi Documentation

--- src/main/java/com/zemka/movieservice/configuration/OpenApiConfiguration.java
+++ src/main/java/com/zemka/movieservice/configuration/OpenApiConfiguration.java
@@ -7,13 +7,14 @@ import io.swagger.v3.oas.annotations.info.Info;
 @OpenAPIDefinition(
         info = @Info(
                 title = "Movie Service using OMDB Database",
-                description = "The Movie Service is a web-based application that leverages the OMDB (Open Movie Database) " +
-                        "to provide comprehensive information about movies and related content. This service allows users " +
-                        "to search for movies, retrieve details about specific films, and access a wealth of data related " +
-                        "to the world of cinema.",
+                description = """
+                        The Movie Service is a web-based application that leverages the OMDB (Open Movie Database) to provide comprehensive information about movies and related content. This service allows users to search for movies, retrieve details about specific films, and access a wealth of data related to the world of cinema.
+
+
+                        [Test 3rd Party Authorization](/login)""",
                 contact = @Contact(
                         name = "Fam Dyk An",
-                        url = "", // TODO
+                        url = "https://scarce-sheet-22a.notion.site/Dyk-An-Fam-f3e56b36882b491ebff3619ca1118171",
                         email = "zemchickpro@gmail.com"
                 ))
 )
